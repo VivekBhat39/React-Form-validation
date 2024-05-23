d26af21ac89b38139e9f472e22de0213c1e485df
Vivek Bhat <VivekBhat39>
2024-05-23 17:57:00 +0530

    mobile Validationi

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -6,7 +6,7 @@ export const signUpSchema = object({
     name: string().min(3).max(25).required("Name is Required"),
     email: string().email().required("Email is Required"),
     // mobile: string().min(10).matches(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/).required("mobile is Required"),
-    mobile: string().min(10).matches(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/, "Mobile must Indian No.").required("mobile is Required"),
+    mobile: string().min(10).matches(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/, "Mobile must be Indian No.").required("mobile is Required"),
     password: string().min(6).required("Password is Required"),
     cpassword: string().required().oneOf([ref('password'), null], "Password must match")
 })  
